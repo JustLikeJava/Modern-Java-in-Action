5674859af31d5887ab9bd83bbb0c8e4ed36106e2
choiwoochang <clllickme@naver.com>
2021-08-13 14:07:38 +0900

    Chapter03_4

---
+++ Src/3장/ChoiWoochang/methodReference/Apple.java
@@ -0,0 +1,16 @@
+public class Apple {
+
+    private Integer weight;
+
+    public Apple(Integer weight) {
+        this.weight = weight;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+}
---
+++ Src/3장/ChoiWoochang/methodReference/AppleComparator.java
@@ -0,0 +1,7 @@
+import java.util.Comparator;
+
+public class AppleComparator implements Comparator<Apple> {
+    public int compare(Apple a1, Apple a2) {
+        return a1.getWeight().compareTo(a2.getWeight());
+    }
+}
